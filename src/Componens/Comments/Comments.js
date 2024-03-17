f9469e23f35af6faa0c9d3ca282d201bb41400c8
Mrx <MRX@email.com>
2024-03-17 20:36:51 +0330

    Get AllComments

--- src/Componens/Comments/Comments.js
+++ src/Componens/Comments/Comments.js
@@ -1,13 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Comments.css"
 import Errorbox from '../Errorbox/Errorbox'
 export default function Comments() {
+
+  const[allComments,setAllComments] = useState([])
+
+  const GetComments =()=>{
+    fetch('http://127.0.0.1:8000/accounts/comments/')
+    .then(res=>res.json())
+    .then(data=>setAllComments(data))
+  }
+  useEffect(()=>{
+    GetComments()
+
+  },[])
   return (
     <div>
-      <Errorbox msg={'هیچ کامنتی یافت نشد'}></Errorbox>
+
+  {allComments.length?(
+        <table className='comment-table'>
+          <thead>
+            <tr>
+              <th>نام</th>
+              <th>ایمیل</th>
+              <th>کامنت</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+          {allComments.map(comment=>{
+            return(
+
+            <tr>
+              <td>{comment.user.username}</td>
+              <td>{comment.user.email}</td>
+              <td>{comment.body}</td>
+              <td className='comment-btn-container'>
+                <button className='btn-show'>نمایش</button>
+                <button className='btn-edit'>ویرایش</button>
+                <button className='btn-del'>حذف</button>
+              </td>
+            </tr>
+            )
+          })}
+
+          </tbody>
+        </table>
+    ):(
+    <Errorbox msg={'هیچ کامنتی یافت نشد'}></Errorbox>
+    )
+    
+  } 
 
 
-        Comments
       
     </div>
   )
